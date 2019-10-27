abfffaee90bf9b27a96f573b23e1411153676b59
Jed Thompson <jedster1111@hotmail.co.uk>
2019-10-27 21:28:00 +0000

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  border: solid 1px black;
+`;
+
+const App: React.FC = () => {
+  return <AppWrapper>Hello</AppWrapper>;
+};
+
+export default App;
